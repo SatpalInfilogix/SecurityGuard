0b4375ffa8c0a7e7af768d51ae4d48211b76393c
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-10-02 17:44:22 +0530

    Created login screen

--- App.tsx
+++ App.tsx
@@ -1,94 +1,108 @@
+// App.tsx
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import AttendanceScreen from './screens/AttendanceScreen';
+import EarnedPayrollScreen from './screens/EarnedPayrollScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import theme from './theme';
-import ProfileScreen from './screens/ProfileScreen';
-import AttendanceScreen from './screens/AttendanceScreen';
-import EarnedPayrollScreen from './screens/EarnedPayrollScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const App = () => {
+const HomeTabs = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarStyle: { 
+        backgroundColor: theme.colors.primary,
+        paddingTop: 10,
+        paddingBottom: 10,
+        height: 50,
+      },
+      tabBarActiveTintColor: theme.colors.secondary,
+      tabBarInactiveTintColor: theme.colors.accent, 
+      headerShown: false
+    }}
+  >
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name={color === theme.colors.secondary ? 'home' : 'home-outline'}
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Attendance"
+      component={AttendanceScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name={color === theme.colors.secondary ? 'calendar-sharp' : 'calendar-outline'}
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="EarnedPayroll"
+      component={EarnedPayrollScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ color, size }) => {
+          const IconComponent = color === theme.colors.secondary ? FontAwesome6 : MaterialIcons;
+          const iconName = color === theme.colors.secondary ? 'money-bills' : 'money';
+          
+          return (
+            <IconComponent
+              name={iconName}
+              color={color}
+              size={size}
+            />
+          );
+        },
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome
+            name={color === theme.colors.secondary ? 'user' : 'user-o'}
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+);
+
+const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-          tabBarStyle: { 
-            backgroundColor: theme.colors.primary,
-            paddingTop: 10,
-            paddingBottom: 10,
-            height: 50,
-          },
-          tabBarActiveTintColor: theme.colors.secondary,
-          tabBarInactiveTintColor: theme.colors.accent, 
-          headerShown: false
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: () => null,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons
-                name={color === theme.colors.secondary ? 'home' : 'home-outline'}
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Attendance"
-          component={AttendanceScreen}
-          options={{
-            tabBarLabel: () => null,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons
-                name={color === theme.colors.secondary ? 'calendar-sharp' : 'calendar-outline'}
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="EarnedPayroll"
-          component={EarnedPayrollScreen}
-          options={{
-            tabBarLabel: () => null,
-            tabBarIcon: ({ color, size }) => {
-              const IconComponent = color === theme.colors.secondary ? FontAwesome6 : MaterialIcons;
-              const iconName = color === theme.colors.secondary ? 'money-bills' : 'money';
-              
-              return (
-                <IconComponent
-                  name={iconName}
-                  color={color}
-                  size={size}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarLabel: () => null,
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesome
-                name={color === theme.colors.secondary ? 'user' : 'user-o'}
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, Button, Text, IconButton, Provider as PaperProvider } from 'react-native-paper';
+import { StackNavigationProp } from '@react-navigation/stack';
+import theme from '../theme';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Make sure to import the icon library you need
+
+type Props = {
+  navigation: StackNavigationProp<any>;
+};
+
+const LoginScreen: React.FC<Props> = ({ navigation }) => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(prevState => !prevState);
+  };
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+    navigation.navigate('HomeTabs');
+  };
+
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          label="Username"
+          value={username}
+          onChangeText={setUsername}
+          style={styles.input}
+          mode="outlined"
+          theme={{
+            colors: {
+              primary: theme.colors.primary
+            },
+          }}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={!showPassword}
+          style={styles.input}
+          mode="outlined"
+          theme={{
+            colors: {
+              primary: theme.colors.primary
+            },
+          }}
+          right={
+            <TextInput.Icon
+                icon={() => (
+                <Icon 
+                    name={showPassword ? 'visibility-off' : 'visibility'} 
+                    size={24} 
+                    onPress={handleTogglePasswordVisibility} 
+                />
+                )}
+                onPress={handleTogglePasswordVisibility}
+            />
+          }
+        />
+        <Button mode="contained" onPress={handleLogin} style={[styles.button, { backgroundColor: theme.colors.secondary }]}>
+          Login
+        </Button>
+      </View>
+    </PaperProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#fff', // or theme.colors.background if preferred
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#000', // or theme.colors.primary if preferred
+  },
+  input: {
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 10,
+  },
+});
+
+export default LoginScreen;
