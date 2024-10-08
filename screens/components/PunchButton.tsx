62a6001ce1e8629f40296d96db76a3df05d1b8d6
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-10-08 11:33:30 +0530

    Make punch in/ punch out functional

--- screens/components/PunchButton.tsx
+++ screens/components/PunchButton.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, PermissionsAndroid, Platform, Alert } from 'react-native';
 import { Button } from 'react-native-paper';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import theme from '../../theme';
 import Geolocation from '@react-native-community/geolocation';
 import { launchCamera, ImagePickerResponse, CameraOptions } from 'react-native-image-picker';
+import axios from 'axios';
+import config from '../../config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const PunchButton: React.FC = () => {
     const [isPunchedIn, setIsPunchedIn] = useState(false);
+    const [isPunching, setIsPunching] = useState(false);
 
     const requestLocationPermission = async () => {
         if (Platform.OS === 'android') {
@@ -62,26 +66,99 @@ const PunchButton: React.FC = () => {
 
         const cameraOptions: CameraOptions = {
             mediaType: 'photo',
-            cameraType: 'front', // To capture a selfie
+            cameraType: 'front',
             saveToPhotos: false,
         };
 
-        launchCamera(cameraOptions, (response: ImagePickerResponse) => {
-            if (response.didCancel) {
-                console.log('User cancelled camera');
-            } else if (response.errorCode) {
+        const getCurrentDateTime = (): string => {
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+            const day = String(now.getDate()).padStart(2, '0');
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            const seconds = String(now.getSeconds()).padStart(2, '0');
+        
+            return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        };
+
+        launchCamera(cameraOptions, async (response: ImagePickerResponse) => {
+            if (response.errorCode) {
                 Alert.alert("Camera Error", response.errorMessage);
                 return;
             } else if (response.assets && response.assets.length > 0) {
-                console.log('assets', response.assets)
-                //setPhoto(response.assets[0].uri);
+                let image = response.assets[0] as any;
+                const prefix = isPunchedIn ? 'out' : 'in';
+                setIsPunching(true);
 
-                Geolocation.getCurrentPosition(info => console.log(info));
-                setIsPunchedIn((prevState) => !prevState);
-                console.log(isPunchedIn ? "Punched Out!" : "Punched In!");
+                try {
+                    const location = await new Promise((resolve, reject) => {
+                        Geolocation.getCurrentPosition(resolve, reject, { enableHighAccuracy: true });
+                    }) as any;
+    
+                    const formData = new FormData();
+                    formData.append(`${prefix}_image`, {
+                        name: image.fileName,
+                        type: image.type,
+                        uri: Platform.OS === 'android' ? image.uri : image.uri.replace('file://', ''),
+                    });
+    
+                    formData.append(`${prefix}_lat`, location.coords.latitude.toString());
+                    formData.append(`${prefix}_long`, location.coords.longitude.toString());
+                    formData.append(`time`, getCurrentDateTime());
+                    formData.append(`${prefix}_location`, JSON.stringify(location));
+                    
+                    const userToken = await AsyncStorage.getItem('@userToken');
+                    
+                    const response = await axios.post(`${config.apiEndpoint}punch/${prefix}`, formData, {
+                        headers: {
+                            'X-Requested-With': 'XMLHttpRequest',
+                            'Authorization': `Bearer ${userToken}`,
+                            'Content-Type': 'multipart/form-data',
+                        },
+                    });
+
+                    let { data } = response.data;
+                    if (prefix === 'in') {
+                        await AsyncStorage.setItem('@punchInfo', JSON.stringify(data));
+                    } else {
+                        await AsyncStorage.removeItem('@punchInfo');
+                    }
+    
+                    setIsPunchedIn((prevState) => !prevState);
+                    setIsPunching(false);
+                } catch (error) {
+                    setIsPunching(false);
+                    if (axios.isAxiosError(error) && error.response?.status) {
+                        console.log('error',error)
+
+                        Alert.alert(error.response.data.message || 'Something went wrong!');
+                    } else {
+                        Alert.alert("Error", `Failed to punch ${prefix}.`);
+                    }
+                    //Alert.alert("Error", "Failed to punch out.");
+                }
             }
         });
     };
+
+    useEffect(()=>{
+        const checkPunchStatus = async () => {
+            try {
+                const punchInfo = await AsyncStorage.getItem('@punchInfo');
+                if (punchInfo) {
+                    setIsPunchedIn(true);
+                } else {
+                    setIsPunchedIn(false);
+                }
+            } catch (error) {
+                console.error('Error retrieving punch status:', error);
+                Alert.alert('Error', 'Failed to retrieve punch status.');
+            }
+        };
+
+        checkPunchStatus();
+    }, [])
 
 
     return (<View style={styles.buttonContainer}>
@@ -93,8 +170,9 @@ const PunchButton: React.FC = () => {
                 style={[styles.button, { backgroundColor: theme.colors.secondary }]}
                 labelStyle={{ color: theme.colors.white }}
                 icon={() => <SimpleLineIcons name="energy" size={20} color={theme.colors.white} />}
+                disabled={isPunching}
             >
-                Punch In
+                {isPunching ? `Please wait` : `Punch In`}
             </Button>
         ) : (
             <Button
@@ -103,8 +181,9 @@ const PunchButton: React.FC = () => {
                 style={[styles.button, { backgroundColor: theme.colors.secondary }]}
                 labelStyle={{ color: theme.colors.white }}
                 icon={() => <SimpleLineIcons name="clock" size={20} color={theme.colors.white} />}
+                disabled={isPunching}
             >
-                Punch Out
+                {isPunching ? `Please wait` : `Punch Out`}
             </Button>
         )}
     </View>);
